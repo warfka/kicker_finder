8ae255fd0fb32e16c784f31a0b3aabc2f426ee14
warfka <egor737614@gmail.com>
2022-06-22 14:07:10 +1000

    переходы между окнами в главном меню и профиль пользователя

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,13 +29,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Start Page'),
+      home: const StartPage(title: 'Start Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -48,22 +49,50 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StartPage> createState() => _StartPageState();
+}
+//---------------------
+class HomePage extends StatefulWidget{
+  const HomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 99;
+class BattlePage extends StatefulWidget{
+  const BattlePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter--;
-    });
-  }
+  @override
+  State<BattlePage> createState() => _BattlePageState();
+}
+
+class TournamentPage extends StatefulWidget{
+  const TournamentPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<TournamentPage> createState() => _TournamentPageState();
+}
+
+class KickerMapPage extends StatefulWidget{
+  const KickerMapPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<KickerMapPage> createState() => _KickerMapPageState();
+}
+
+class PlayerProfilePage extends StatefulWidget{
+  const PlayerProfilePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<PlayerProfilePage> createState() => _PlayerProfilePageState();
+}
+
+class _StartPageState extends State<StartPage> {
 
   @override
   Widget build(BuildContext context) {
@@ -116,32 +145,208 @@ class _MyHomePageState extends State<MyHomePage> {
                   fontFamily: 'BalsamiqSans'
               ),
             ),
-            Container(
-                child: Link(
-                  target: LinkTarget.self,
-                  uri: Uri.parse('https://github.com/warfka/kicker_finder'),
-                  builder: (context, followLink) => GestureDetector(
-                    onTap: followLink,
-                    child: Text(
-                      'https://github.com/warfka/kicker_finder',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.red,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                  ),
+            TextButton(
+                onPressed: () async{
+                  final Uri url = Uri.parse('https://github.com/warfka/kicker_finder');
+                  if (!await launchUrl(url)) throw 'Could not launch $url';
+                },
+              child: Text(
+                'https://github.com/warfka/kicker_finder',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.red,
+                  decoration: TextDecoration.underline,
                 ),
+              ),
+            ),
+            RawMaterialButton(
+                onPressed: () {
+                  //final String title;
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(title: 'Home Page')));
+                },
+              fillColor: Colors.red[400],
+              child: Text('Нажмите чтобы начать'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class _HomePageState extends State<HomePage>{
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            OutlinedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => BattlePage(title: 'Batle Page')));
+                },
+                child: Text('1x1'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => TournamentPage(title: 'Tournament Page')));
+              },
+              child: Text('Турнир'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => KickerMapPage(title: 'Kicker Map Page')));
+              },
+              child: Text('Карта площадок'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => PlayerProfilePage(title: 'Player Profile Page')));
+              },
+              child: Text('Профиль'),
             ),
           ],
         ),
 
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.account_circle_rounded),
+        onPressed: () {
+
+        },
+        tooltip: 'Setings',
+        child: const Icon(Icons.adb),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _BattlePageState extends State<BattlePage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TournamentPageState extends State<TournamentPage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _KickerMapPageState extends State<KickerMapPage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerProfilePageState extends State<PlayerProfilePage>{
+
+  List<String> _arrayPlayerProfiles = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Image.network(
+                "https://www.images.lesyadraw.ru/2014/07/kak_narisovat_aang_avatar0.jpg"
+            ),
+            Text(
+              'Любитель Кикера' ,
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                  fontFamily: 'BalsamiqSans'
+              ),
+            ),
+            new ListView.builder( //ToDo Best players list
+              itemBuilder: (context, index) {
+                return new Text('Player $index');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
